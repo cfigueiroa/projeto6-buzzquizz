87edd1573ebf6a5e38884d95cdcabca662788d00
cfigueiroa <cfigueiroa@gmail.com>
2022-11-02 12:52:11 -0300

    feat: add dynamic quizzes list

--- js/script.js
+++ js/script.js
@@ -1,4 +1,34 @@
 const api = 'https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/';
 //page1
+
+function loadQuizzes() {
+    axios.get(api)
+        .then(function (response) {
+            console.log(response.data);
+            const quizzes = response.data;
+            const cards = document.querySelector('.bot .cards');
+            cards.innerHTML = '';
+
+            for (let i = 0; i < quizzes.length; i++) {
+                const newDiv = document.createElement("div");
+                newDiv.classList.add('card');
+                newDiv.innerHTML = `<p>${quizzes[i].title}</p>`;
+                newDiv.style.background = `linear-gradient(
+                    180deg,
+                    rgba(255, 255, 255, 0) 0%,
+                    rgba(0, 0, 0, 0.5) 64.58%,
+                    #000000 100%
+                  ),url(${quizzes[i].image})`;
+                cards.appendChild(newDiv);
+            }
+
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+
+loadQuizzes();
+
 //page2
 //page3
