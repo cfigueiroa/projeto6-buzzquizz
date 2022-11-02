ffcdc7cffdebf40e1141c8832b9fb858bf5989c0
denilsonslima <denilsondslima@gmail.com>
2022-11-02 13:34:48 -0300

    added letter click function page2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
 const api = 'https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/';
 //page1
 //page2
+function selecionarCaixa(seletor){
+    const caixaClicada = seletor.parentNode;
+    const p = caixaClicada.querySelector("p")
+    caixaClicada.classList.add("clicado")
+    p.classList.add("acertou")
+
+    const caixas = document.querySelectorAll(".caixa")
+    for (let i = 0; i < caixas.length; i++){
+        if(caixas[i].classList.contains("clicado") === false){
+            caixas[i].innerHTML+= `
+            <div class="esconder"></div>
+            `
+        }
+        else {
+            seletor.removeAttribute("onclick")
+        }
+    }
+}
 //page3
