e0b49f076bb6b95617e77ca5a28fad27416cf15f
denilsonslima <denilsondslima@gmail.com>
2022-11-03 00:19:35 -0300

    Added create quiz function

--- js/script.js
+++ js/script.js
@@ -69,14 +69,14 @@ loadQuizzes();
 
 
 //page2
-let contador = 0;
 function selecionarCaixa(seletor) {
     const caixaClicada = seletor.parentNode;
+    const click = caixaClicada.parentNode;
     const p = caixaClicada.querySelector("p")
     caixaClicada.classList.add("clicado")
     p.classList.add("acertou")
 
-    const caixas = document.querySelectorAll(`#caixa${contador} .caixa`)
+    const caixas = click.querySelectorAll(`.caixa`)
     for (let i = 0; i < caixas.length; i++) {
         if (caixas[i].classList.contains("clicado") === false) {
             caixas[i].innerHTML += `
@@ -85,58 +85,43 @@ function selecionarCaixa(seletor) {
         }
         else {
             seletor.removeAttribute("onclick")
-            contador++
         }
     }
 }
 
 function criarQuizz() {
     // Alterando o Fundo
-    const fundo = document.querySelector(".fundo")
-    let title = currentObj.title
-    let back = currentObj.image
-    fundo.style.backgroundImage = `url("${back}")`;
+    const fundo = document.querySelector(".fundo");
+    fundo.style.backgroundImage = `url("${currentObj.image}")`;
     fundo.style.backgroundSize = 'cover';
-    fundo.innerHTML = title;
-    back = "";
-    title = "";
-
+    fundo.innerHTML = currentObj.title;
 
     const container = document.querySelector(".container")
     container.innerHTML = ""
     
-    for (let i = 0; i < currentObj.questions.length; i++) {
+    let contador = currentObj.questions.length;
+    for (let i = 0; i < contador; i++) {
         container.innerHTML += `
         <div class="quizz-caixa" id="caixa${i}">
             <div id="titulo${i}" class="titulo-quizz">${currentObj.questions[i].title}</div>
-            <div class="conteudo-quizz">
-              <div class="caixa">
-                <div onclick="selecionarCaixa(this)" class="img" id="img${0}"></div>
-                <p id="paragrafo${0}">gatinho</p>
-              </div>
-              <div class="caixa">
-                <div onclick="selecionarCaixa(this)" class="img" id="img${1}"></div>
-                <p id="paragrafo${1}">gatinho</p>
-              </div>
-              <div class="caixa">
-                <div onclick="selecionarCaixa(this)" class="img" id="img${2}"></div>
-                <p id="paragrafo${2}">gatinho</p>
-              </div>
-              <div class="caixa">
-                <div onclick="selecionarCaixa(this)" class="img" id="img${3}"></div>
-                <p id="paragrafo${3}">gatinho</p>
-              </div>
+            <div class="conteudo-quizz">         
             </div>
           </div>
         `
-        for (let a = 0; a < 4; a++){
-            let imagem = document.querySelector(`#caixa${i} #img${a}`)
-            imagem.style.backgroundImage = `url("${currentObj.questions[i].answers[a].image}")`
+        let caixas = document.querySelector(`#caixa${i} .conteudo-quizz`);
+        for(let j = 0; j < currentObj.questions[i].answers.length; j++){
+            caixas.innerHTML+= `
+            <div class="caixa">
+                <div onclick="selecionarCaixa(this)" class="img" id="img${j}"></div>
+                <p id="paragrafo${j}">gatinho</p>
+            </div>
+        ` 
+            let imagem = document.querySelector(`#caixa${i} #img${j}`)
+            imagem.style.backgroundImage = `url("${currentObj.questions[i].answers[j].image}")`
             imagem.style.backgroundSize = '100% 100%';
-            let paragrafo = document.querySelector(`#caixa${i} #paragrafo${a}`)
-            paragrafo.innerHTML = `${currentObj.questions[i].answers[a].text}`
+            let paragrafo = document.querySelector(`#caixa${i} #paragrafo${j}`)
+            paragrafo.innerHTML = `${currentObj.questions[i].answers[j].text}`
         }
-
         let titulo = document.querySelector(`#titulo${i}`)
         titulo.style.backgroundColor = currentObj.questions[i].color
         titulo = ""
